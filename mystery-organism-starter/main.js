42c6b6a0e7fbd077e57271c36705807fb580ce03
Kallum Tolkien <kallumtolkien@gmail.com>
2021-06-01 23:49:35 +0100

    adds mutate method and  tests for factory function

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -1,23 +1,46 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
-};
+  const dnaBases = ['A', 'T', 'C', 'G']
+  return dnaBases[Math.floor(Math.random() * 4)] 
+}
 
-// Returns a random single stand of DNA containing 15 bases
+// Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
-  const newStrand = [];
+  const newStrand = []
   for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
+    newStrand.push(returnRandBase())
   }
-  return newStrand;
-};
+  return newStrand
+}
 
-console.log(returnRandBase());
-console.log(mockUpStrand());
+// Pila Aequer factory function
+const pAequorFactory = (num, dna) => {
+  return {
+    specimenNum: num,
+    dna: dna,
+    mutate() {
+      const randIndex = Math.floor(Math.random() * this.dna.length);
+      let newBase = returnRandBase();
+      while (this.dna[randIndex] === newBase) {
+        newBase = returnRandBase();
+      }
+      this.dna[randIndex] = newBase;
+      return this.dna;
+    }
+  }
+}
 
 
 
+
+
+// Console logs
+// console.log(returnRandBase());
+// console.log(mockUpStrand());
+// console.log(pAequorFactory(1, mockUpStrand()));
+let pA1 = pAequorFactory(1, mockUpStrand());
+console.log(pA1.dna);
+console.log(pA1.mutate());
 
 
 
