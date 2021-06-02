41be7700cef34e7ec37fec2ae9b8b63680773d5d
Kallum Tolkien <kallumtolkien@gmail.com>
2021-06-02 23:44:51 +0100

    adds compare DNA method

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -26,9 +26,24 @@ const pAequorFactory = (num, dna) => {
       }
       this.dna[randIndex] = newBase;
       return this.dna;
+    },
+    compareDNA(pA2) {
+      let pA1dna = this.dna;
+      let pA2dna = pA2.dna;
+      let sameBaseCount = 0;
+      console.log(pA1dna);
+      console.log(pA2dna);
+      for (let i = 0; i < pA1dna.length; i++) {
+        if (pA1dna[i] == pA2dna[i]) {
+          sameBaseCount += 1;
+        };
+      }
+      console.log(sameBaseCount);
+      let percentSimilar = (sameBaseCount / pA1dna.length) * 100;
+      return `specimen #1 and specimen #2 have ${percentSimilar}% DNA in common`;
+      }
     }
   }
-}
 
 
 
@@ -39,9 +54,10 @@ const pAequorFactory = (num, dna) => {
 // console.log(mockUpStrand());
 // console.log(pAequorFactory(1, mockUpStrand()));
 let pA1 = pAequorFactory(1, mockUpStrand());
-console.log(pA1.dna);
-console.log(pA1.mutate());
-
+let pA2 = pAequorFactory(2, mockUpStrand())
+// console.log(pA1.dna);
+// console.log(pA1.mutate());
+console.log(pA1.compareDNA(pA2));
 
 
 
